a2c03d5dc7bbf6080f01ea688354fad1f15e15ad
Adam Kamienski <adam.kamienski@accenture.com>
2024-03-17 21:49:48 +0100

    sab registry

--- main-pipeline.ps1
+++ main-pipeline.ps1
@@ -71,5 +71,91 @@ Remove-Item -Recurse -Force ProcessKiller
 
 ## StartAllBack
 
-$registryFile = $pwd + '\StartAllBack\StartAllBack.reg'
-Start-Process -FilePath 'regedit.exe' -ArgumentList "/s $RegistryFile" -Wait
+# $registryFile = $pwd + '\StartAllBack\StartAllBack.reg'
+# Start-Process -FilePath 'regedit.exe' -ArgumentList "/s $RegistryFile" -Wait
+
+$registryPath = "HKCU:\Software\StartIsBack"
+
+Set-ItemProperty -Path $registryPath -Name "WinBuild" -Value 0x5867
+Set-ItemProperty -Path $registryPath -Name "WinLangID" -Value 0x0409
+Set-ItemProperty -Path $registryPath -Name "ModernIconsColorized" -Value 0
+Set-ItemProperty -Path $registryPath -Name "SettingsVersion" -Value 5
+Set-ItemProperty -Path $registryPath -Name "WelcomeShown" -Value 3
+Set-ItemProperty -Path $registryPath -Name "UpdateCheck" -Value ([byte[]](0x44, 0xCE, 0xBE, 0x05, 0x25, 0x77, 0xDA, 0x01))
+Set-ItemProperty -Path $registryPath -Name "FrameStyle" -Value 2
+Set-ItemProperty -Path $registryPath -Name "OrbBitmap" -Value ""
+Set-ItemProperty -Path $registryPath -Name "AlterStyle" -Value ""
+Set-ItemProperty -Path $registryPath -Name "TaskbarStyle" -Value ""
+Set-ItemProperty -Path $registryPath -Name "SysTrayStyle" -Value 1
+Set-ItemProperty -Path $registryPath -Name "BottomDetails" -Value 0
+Set-ItemProperty -Path $registryPath -Name "RestyleIcons" -Value 1
+Set-ItemProperty -Path $registryPath -Name "NavBarGlass" -Value 1
+Set-ItemProperty -Path $registryPath -Name "OldSearch" -Value 1
+Set-ItemProperty -Path $registryPath -Name "NoXAMLMenus" -Value 1
+Set-ItemProperty -Path $registryPath -Name "RestyleControls" -Value 0
+Set-ItemProperty -Path $registryPath -Name "WinkeyFunction" -Value 0
+Set-ItemProperty -Path $registryPath -Name "TaskbarJumpList" -Value 1
+Set-ItemProperty -Path $registryPath -Name "TaskbarOneSegment" -Value 0
+Set-ItemProperty -Path $registryPath -Name "TaskbarCenterIcons" -Value 1
+Set-ItemProperty -Path $registryPath -Name "TaskbarTranslucentEffect" -Value 1
+Set-ItemProperty -Path $registryPath -Name "SysTrayActionCenter" -Value 0
+Set-ItemProperty -Path $registryPath -Name "TaskbarLargerIcons" -Value 0
+Set-ItemProperty -Path $registryPath -Name "UndeadControlPanel" -Value 1
+Set-ItemProperty -Path $registryPath -Name "LegacyTaskbar" -Value 1
+Set-ItemProperty -Path $registryPath -Name "TaskbarSpacierIcons" -Value -1
+Set-ItemProperty -Path $registryPath -Name "SysTrayNetwork" -Value 1
+Set-ItemProperty -Path $registryPath -Name "SysTrayClockFormat" -Value 3
+Set-ItemProperty -Path $registryPath -Name "TaskbarControlCenter" -Value 1
+Set-ItemProperty -Path $registryPath -Name "SysTrayVolume" -Value 1
+Set-ItemProperty -Path $registryPath -Name "SysTrayPower" -Value 1
+Set-ItemProperty -Path $registryPath -Name "CustomColors" -Value @{
+    "ColorA" = "FFFFFFFF"
+    "ColorB" = "FFFFFFFF"
+    "ColorC" = "FFFFFFFF"
+    "ColorD" = "FFFFFFFF"
+    "ColorE" = "FFFFFFFF"
+    "ColorF" = "FFFFFFFF"
+    "ColorG" = "FFFFFFFF"
+    "ColorH" = "FFFFFFFF"
+    "ColorI" = "FFFFFFFF"
+    "ColorJ" = "FFFFFFFF"
+    "ColorK" = "FFFFFFFF"
+    "ColorL" = "FFFFFFFF"
+    "ColorM" = "FFFFFFFF"
+    "ColorN" = "FFFFFFFF"
+    "ColorO" = "FFFFFFFF"
+    "ColorP" = "FFFFFFFF"
+}
+Set-ItemProperty -Path $registryPath -Name "Start_LargeAllAppsIcons" -Value 0
+Set-ItemProperty -Path $registryPath -Name "AllProgramsFlyout" -Value 1
+Set-ItemProperty -Path $registryPath -Name "StartMetroAppsFolder" -Value 1
+Set-ItemProperty -Path $registryPath -Name "Start_SortOverride" -Value 0
+Set-ItemProperty -Path $registryPath -Name "Start_NotifyNewApps" -Value 1
+Set-ItemProperty -Path $registryPath -Name "Start_AutoCascade" -Value 0
+Set-ItemProperty -Path $registryPath -Name "Start_AskCortana" -Value 0
+Set-ItemProperty -Path $registryPath -Name "HideUserFrame" -Value 1
+Set-ItemProperty -Path $registryPath -Name "Start_RightPaneIcons" -Value 0
+Set-ItemProperty -Path $registryPath -Name "Start_ShowUser" -Value 1
+Set-ItemProperty -Path $registryPath -Name "Start_ShowMyDocs" -Value 1
+Set-ItemProperty -Path $registryPath -Name "Start_ShowMyPics" -Value 1
+Set-ItemProperty -Path $registryPath -Name "Start_ShowMyMusic" -Value 0
+Set-ItemProperty -Path $registryPath -Name "Start_ShowVideos" -Value 1
+Set-ItemProperty -Path $registryPath -Name "Start_ShowDownloads" -Value 1
+Set-ItemProperty -Path $registryPath -Name "Start_ShowSkyDrive" -Value 1
+Set-ItemProperty -Path $registryPath -Name "StartMenuFavorites" -Value 0
+Set-ItemProperty -Path $registryPath -Name "Start_ShowRecentDocs" -Value 0
+Set-ItemProperty -Path $registryPath -Name "Start_ShowNetPlaces" -Value 0
+Set-ItemProperty -Path $registryPath -Name "Start_ShowNetConn" -Value 0
+Set-ItemProperty -Path $registryPath -Name "Start_ShowMyComputer" -Value 1
+Set-ItemProperty -Path $registryPath -Name "Start_ShowControlPanel" -Value 0
+Set-ItemProperty -Path $registryPath -Name "Start_ShowPCSettings" -Value 0
+Set-ItemProperty -Path $registryPath -Name "Start_AdminToolsRoot" -Value 0
+Set-ItemProperty -Path $registryPath -Name "Start_ShowPrinters" -Value 0
+Set-ItemProperty -Path $registryPath -Name "Start_ShowSetProgramAccessAndDefaults" -Value 0
+Set-ItemProperty -Path $registryPath -Name "Start_ShowTerminal" -Value 0
+Set-ItemProperty -Path $registryPath -Name "Start_ShowCommandPrompt" -Value 0
+Set-ItemProperty -Path $registryPath -Name "Start_ShowRun" -Value 0
+Set-ItemProperty -Path $registryPath -Name "Start_MinMFU" -Value 14
+Set-ItemProperty -Path $registryPath -Name "SysTrayCopilotIcon" -Value 1
+Set-ItemProperty -Path $registryPath -Name "MultiColumnFlyout" -Value 0
+Set-ItemProperty -Path $registryPath -Name "Start_LargeMFUIcons" -Value 0
