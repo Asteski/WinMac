6c9faf333728d5df753b0677d90446da8ba410a4
Adams <adam.kamienski@accenture.com>
2024-03-18 19:35:11 +0100

    add sab reg entries

--- main-pipeline.ps1
+++ main-pipeline.ps1
@@ -89,106 +89,106 @@ Write-Host "Installing Packages completed." -ForegroundColor Green
 
 Write-Host "Configuring StartAllBack..." -ForegroundColor Yellow
 
-# $exPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\"
-# $sabPath = "HKCU:\Software\StartIsBack"
-# $cachePath = "HKCU:\Software\StartIsBack\Cache"
+$exPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\"
+$sabPath = "HKCU:\Software\StartIsBack"
+$cachePath = "HKCU:\Software\StartIsBack\Cache"
 
-# Set-ItemProperty -Path $exPath\StuckRects3 -Name "Settings" -Value ([byte[]]@(1,0,0,0,0,0,0,0,1,0,0,0,3,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,64,18,0,0,0,0,0,0,4,0,0,0,10,0,0,0,01,00,00,00))
-# Set-ItemProperty -Path $exPath\Advanced -Name "TaskbarSi" -Value 0
-# Set-ItemProperty -Path $sabPath -Name "WinBuild" -Value 0x5867
-# Set-ItemProperty -Path $sabPath -Name "WinLangID" -Value 0x0409
-# Set-ItemProperty -Path $sabPath -Name "ModernIconsColorized" -Value 0
-# Set-ItemProperty -Path $sabPath -Name "SettingsVersion" -Value 5
-# Set-ItemProperty -Path $sabPath -Name "WelcomeShown" -Value 3
-# Set-ItemProperty -Path $sabPath -Name "UpdateCheck" -Value ([byte[]](0x44, 0xCE, 0xBE, 0x05, 0x25, 0x77, 0xDA, 0x01))
-# Set-ItemProperty -Path $sabPath -Name "FrameStyle" -Value 2
-# Set-ItemProperty -Path $sabPath -Name "OrbBitmap" -Value ""
-# Set-ItemProperty -Path $sabPath -Name "AlterStyle" -Value ""
-# Set-ItemProperty -Path $sabPath -Name "TaskbarStyle" -Value ""
-# Set-ItemProperty -Path $sabPath -Name "SysTrayStyle" -Value 1
-# Set-ItemProperty -Path $sabPath -Name "BottomDetails" -Value 0
-# Set-ItemProperty -Path $sabPath -Name "RestyleIcons" -Value 1
-# Set-ItemProperty -Path $sabPath -Name "NavBarGlass" -Value 1
-# Set-ItemProperty -Path $sabPath -Name "OldSearch" -Value 1
-# Set-ItemProperty -Path $sabPath -Name "NoXAMLMenus" -Value 1
-# Set-ItemProperty -Path $sabPath -Name "RestyleControls" -Value 0
-# Set-ItemProperty -Path $sabPath -Name "WinkeyFunction" -Value 0
-# Set-ItemProperty -Path $sabPath -Name "TaskbarJumpList" -Value 1
-# Set-ItemProperty -Path $sabPath -Name "TaskbarOneSegment" -Value 0
-# Set-ItemProperty -Path $sabPath -Name "TaskbarCenterIcons" -Value 1
-# Set-ItemProperty -Path $sabPath -Name "TaskbarTranslucentEffect" -Value 0
-# Set-ItemProperty -Path $sabPath -Name "SysTrayActionCenter" -Value 0
-# Set-ItemProperty -Path $sabPath -Name "TaskbarLargerIcons" -Value 0
-# Set-ItemProperty -Path $sabPath -Name "UndeadControlPanel" -Value 1
-# Set-ItemProperty -Path $sabPath -Name "LegacyTaskbar" -Value 1
-# Set-ItemProperty -Path $sabPath -Name "TaskbarSpacierIcons" -Value -1
-# Set-ItemProperty -Path $sabPath -Name "SysTrayNetwork" -Value 1
-# Set-ItemProperty -Path $sabPath -Name "SysTrayClockFormat" -Value 3
-# Set-ItemProperty -Path $sabPath -Name "TaskbarControlCenter" -Value 1
-# Set-ItemProperty -Path $sabPath -Name "SysTrayVolume" -Value 1
-# Set-ItemProperty -Path $sabPath -Name "SysTrayPower" -Value 1
-# Set-ItemProperty -Path $sabPath -Name "CustomColors" -Value @{
-#     "ColorA" = "FFFFFFFF"
-#     "ColorB" = "FFFFFFFF"
-#     "ColorC" = "FFFFFFFF"
-#     "ColorD" = "FFFFFFFF"
-#     "ColorE" = "FFFFFFFF"
-#     "ColorF" = "FFFFFFFF"
-#     "ColorG" = "FFFFFFFF"
-#     "ColorH" = "FFFFFFFF"
-#     "ColorI" = "FFFFFFFF"
-#     "ColorJ" = "FFFFFFFF"
-#     "ColorK" = "FFFFFFFF"
-#     "ColorL" = "FFFFFFFF"
-#     "ColorM" = "FFFFFFFF"
-#     "ColorN" = "FFFFFFFF"
-#     "ColorO" = "FFFFFFFF"
-#     "ColorP" = "FFFFFFFF"
-# }
-# Set-ItemProperty -Path $sabPath -Name "Start_LargeAllAppsIcons" -Value 0
-# Set-ItemProperty -Path $sabPath -Name "AllProgramsFlyout" -Value 1
-# Set-ItemProperty -Path $sabPath -Name "StartMetroAppsFolder" -Value 1
-# Set-ItemProperty -Path $sabPath -Name "Start_SortOverride" -Value 0
-# Set-ItemProperty -Path $sabPath -Name "Start_NotifyNewApps" -Value 1
-# Set-ItemProperty -Path $sabPath -Name "Start_AutoCascade" -Value 0
-# Set-ItemProperty -Path $sabPath -Name "Start_AskCortana" -Value 0
-# Set-ItemProperty -Path $sabPath -Name "HideUserFrame" -Value 1
-# Set-ItemProperty -Path $sabPath -Name "Start_RightPaneIcons" -Value 0
-# Set-ItemProperty -Path $sabPath -Name "Start_ShowUser" -Value 1
-# Set-ItemProperty -Path $sabPath -Name "Start_ShowMyDocs" -Value 1
-# Set-ItemProperty -Path $sabPath -Name "Start_ShowMyPics" -Value 1
-# Set-ItemProperty -Path $sabPath -Name "Start_ShowMyMusic" -Value 0
-# Set-ItemProperty -Path $sabPath -Name "Start_ShowVideos" -Value 1
-# Set-ItemProperty -Path $sabPath -Name "Start_ShowDownloads" -Value 1
-# Set-ItemProperty -Path $sabPath -Name "Start_ShowSkyDrive" -Value 1
-# Set-ItemProperty -Path $sabPath -Name "StartMenuFavorites" -Value 0
-# Set-ItemProperty -Path $sabPath -Name "Start_ShowRecentDocs" -Value 0
-# Set-ItemProperty -Path $sabPath -Name "Start_ShowNetPlaces" -Value 0
-# Set-ItemProperty -Path $sabPath -Name "Start_ShowNetConn" -Value 0
-# Set-ItemProperty -Path $sabPath -Name "Start_ShowMyComputer" -Value 1
-# Set-ItemProperty -Path $sabPath -Name "Start_ShowControlPanel" -Value 0
-# Set-ItemProperty -Path $sabPath -Name "Start_ShowPCSettings" -Value 0
-# Set-ItemProperty -Path $sabPath -Name "Start_AdminToolsRoot" -Value 0
-# Set-ItemProperty -Path $sabPath -Name "Start_ShowPrinters" -Value 0
-# Set-ItemProperty -Path $sabPath -Name "Start_ShowSetProgramAccessAndDefaults" -Value 0
-# Set-ItemProperty -Path $sabPath -Name "Start_ShowTerminal" -Value 0
-# Set-ItemProperty -Path $sabPath -Name "Start_ShowCommandPrompt" -Value 0
-# Set-ItemProperty -Path $sabPath -Name "Start_ShowRun" -Value 0
-# Set-ItemProperty -Path $sabPath -Name "Start_MinMFU" -Value 14
-# Set-ItemProperty -Path $sabPath -Name "SysTrayCopilotIcon" -Value 1
-# Set-ItemProperty -Path $sabPath -Name "MultiColumnFlyout" -Value 0
-# Set-ItemProperty -Path $sabPath -Name "Start_LargeMFUIcons" -Value 0
+Set-ItemProperty -Path $exPath\StuckRects3 -Name "Settings" -Value ([byte[]]@(1,0,0,0,0,0,0,0,1,0,0,0,3,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,64,18,0,0,0,0,0,0,4,0,0,0,10,0,0,0,01,00,00,00))
+Set-ItemProperty -Path $exPath\Advanced -Name "TaskbarSi" -Value 0
+Set-ItemProperty -Path $sabPath -Name "WinBuild" -Value 0x5867
+Set-ItemProperty -Path $sabPath -Name "WinLangID" -Value 0x0409
+Set-ItemProperty -Path $sabPath -Name "ModernIconsColorized" -Value 0
+Set-ItemProperty -Path $sabPath -Name "SettingsVersion" -Value 5
+Set-ItemProperty -Path $sabPath -Name "WelcomeShown" -Value 3
+Set-ItemProperty -Path $sabPath -Name "UpdateCheck" -Value ([byte[]](0x44, 0xCE, 0xBE, 0x05, 0x25, 0x77, 0xDA, 0x01))
+Set-ItemProperty -Path $sabPath -Name "FrameStyle" -Value 2
+Set-ItemProperty -Path $sabPath -Name "OrbBitmap" -Value ""
+Set-ItemProperty -Path $sabPath -Name "AlterStyle" -Value ""
+Set-ItemProperty -Path $sabPath -Name "TaskbarStyle" -Value ""
+Set-ItemProperty -Path $sabPath -Name "SysTrayStyle" -Value 1
+Set-ItemProperty -Path $sabPath -Name "BottomDetails" -Value 0
+Set-ItemProperty -Path $sabPath -Name "RestyleIcons" -Value 1
+Set-ItemProperty -Path $sabPath -Name "NavBarGlass" -Value 1
+Set-ItemProperty -Path $sabPath -Name "OldSearch" -Value 1
+Set-ItemProperty -Path $sabPath -Name "NoXAMLMenus" -Value 1
+Set-ItemProperty -Path $sabPath -Name "RestyleControls" -Value 0
+Set-ItemProperty -Path $sabPath -Name "WinkeyFunction" -Value 0
+Set-ItemProperty -Path $sabPath -Name "TaskbarJumpList" -Value 1
+Set-ItemProperty -Path $sabPath -Name "TaskbarOneSegment" -Value 0
+Set-ItemProperty -Path $sabPath -Name "TaskbarCenterIcons" -Value 1
+Set-ItemProperty -Path $sabPath -Name "TaskbarTranslucentEffect" -Value 0
+Set-ItemProperty -Path $sabPath -Name "SysTrayActionCenter" -Value 0
+Set-ItemProperty -Path $sabPath -Name "TaskbarLargerIcons" -Value 0
+Set-ItemProperty -Path $sabPath -Name "UndeadControlPanel" -Value 1
+Set-ItemProperty -Path $sabPath -Name "LegacyTaskbar" -Value 1
+Set-ItemProperty -Path $sabPath -Name "TaskbarSpacierIcons" -Value -1
+Set-ItemProperty -Path $sabPath -Name "SysTrayNetwork" -Value 1
+Set-ItemProperty -Path $sabPath -Name "SysTrayClockFormat" -Value 3
+Set-ItemProperty -Path $sabPath -Name "TaskbarControlCenter" -Value 1
+Set-ItemProperty -Path $sabPath -Name "SysTrayVolume" -Value 1
+Set-ItemProperty -Path $sabPath -Name "SysTrayPower" -Value 1
+Set-ItemProperty -Path $sabPath -Name "CustomColors" -Value @{
+    "ColorA" = "FFFFFFFF"
+    "ColorB" = "FFFFFFFF"
+    "ColorC" = "FFFFFFFF"
+    "ColorD" = "FFFFFFFF"
+    "ColorE" = "FFFFFFFF"
+    "ColorF" = "FFFFFFFF"
+    "ColorG" = "FFFFFFFF"
+    "ColorH" = "FFFFFFFF"
+    "ColorI" = "FFFFFFFF"
+    "ColorJ" = "FFFFFFFF"
+    "ColorK" = "FFFFFFFF"
+    "ColorL" = "FFFFFFFF"
+    "ColorM" = "FFFFFFFF"
+    "ColorN" = "FFFFFFFF"
+    "ColorO" = "FFFFFFFF"
+    "ColorP" = "FFFFFFFF"
+}
+Set-ItemProperty -Path $sabPath -Name "Start_LargeAllAppsIcons" -Value 0
+Set-ItemProperty -Path $sabPath -Name "AllProgramsFlyout" -Value 1
+Set-ItemProperty -Path $sabPath -Name "StartMetroAppsFolder" -Value 1
+Set-ItemProperty -Path $sabPath -Name "Start_SortOverride" -Value 0
+Set-ItemProperty -Path $sabPath -Name "Start_NotifyNewApps" -Value 1
+Set-ItemProperty -Path $sabPath -Name "Start_AutoCascade" -Value 0
+Set-ItemProperty -Path $sabPath -Name "Start_AskCortana" -Value 0
+Set-ItemProperty -Path $sabPath -Name "HideUserFrame" -Value 1
+Set-ItemProperty -Path $sabPath -Name "Start_RightPaneIcons" -Value 0
+Set-ItemProperty -Path $sabPath -Name "Start_ShowUser" -Value 1
+Set-ItemProperty -Path $sabPath -Name "Start_ShowMyDocs" -Value 1
+Set-ItemProperty -Path $sabPath -Name "Start_ShowMyPics" -Value 1
+Set-ItemProperty -Path $sabPath -Name "Start_ShowMyMusic" -Value 0
+Set-ItemProperty -Path $sabPath -Name "Start_ShowVideos" -Value 1
+Set-ItemProperty -Path $sabPath -Name "Start_ShowDownloads" -Value 1
+Set-ItemProperty -Path $sabPath -Name "Start_ShowSkyDrive" -Value 1
+Set-ItemProperty -Path $sabPath -Name "StartMenuFavorites" -Value 0
+Set-ItemProperty -Path $sabPath -Name "Start_ShowRecentDocs" -Value 0
+Set-ItemProperty -Path $sabPath -Name "Start_ShowNetPlaces" -Value 0
+Set-ItemProperty -Path $sabPath -Name "Start_ShowNetConn" -Value 0
+Set-ItemProperty -Path $sabPath -Name "Start_ShowMyComputer" -Value 1
+Set-ItemProperty -Path $sabPath -Name "Start_ShowControlPanel" -Value 0
+Set-ItemProperty -Path $sabPath -Name "Start_ShowPCSettings" -Value 0
+Set-ItemProperty -Path $sabPath -Name "Start_AdminToolsRoot" -Value 0
+Set-ItemProperty -Path $sabPath -Name "Start_ShowPrinters" -Value 0
+Set-ItemProperty -Path $sabPath -Name "Start_ShowSetProgramAccessAndDefaults" -Value 0
+Set-ItemProperty -Path $sabPath -Name "Start_ShowTerminal" -Value 0
+Set-ItemProperty -Path $sabPath -Name "Start_ShowCommandPrompt" -Value 0
+Set-ItemProperty -Path $sabPath -Name "Start_ShowRun" -Value 0
+Set-ItemProperty -Path $sabPath -Name "Start_MinMFU" -Value 14
+Set-ItemProperty -Path $sabPath -Name "SysTrayCopilotIcon" -Value 1
+Set-ItemProperty -Path $sabPath -Name "MultiColumnFlyout" -Value 0
+Set-ItemProperty -Path $sabPath -Name "Start_LargeMFUIcons" -Value 0
 
-# Set-ItemProperty -Path $cachePath -Name "OrbWidth.120" -Value 0x00000027
-# Set-ItemProperty -Path $cachePath -Name "OrbHeight.120" -Value 0x00000026
-# Set-ItemProperty -Path $cachePath -Name "IdealHeight.6" -Value 0x00000000
-# Set-ItemProperty -Path $cachePath -Name "IdealHeight.9" -Value 0x00010007
-# Set-ItemProperty -Path $cachePath -Name "IdealWidth.9" -Value "OneDrive"
-# Set-ItemProperty -Path $cachePath -Name "OrbWidth.96" -Value 0x00000020
-# Set-ItemProperty -Path $cachePath -Name "OrbHeight.96" -Value 0x0000001e
-# Set-ItemProperty -Path $cachePath -Name "IdealHeight.7" -Value 0x00000000
-# Set-ItemProperty -Path $cachePath -Name "OrbWidth.144" -Value 0x00000030
-# Set-ItemProperty -Path $cachePath -Name "OrbHeight.144" -Value 0x0000002e
+Set-ItemProperty -Path $cachePath -Name "OrbWidth.120" -Value 0x00000027
+Set-ItemProperty -Path $cachePath -Name "OrbHeight.120" -Value 0x00000026
+Set-ItemProperty -Path $cachePath -Name "IdealHeight.6" -Value 0x00000000
+Set-ItemProperty -Path $cachePath -Name "IdealHeight.9" -Value 0x00010007
+Set-ItemProperty -Path $cachePath -Name "IdealWidth.9" -Value "OneDrive"
+Set-ItemProperty -Path $cachePath -Name "OrbWidth.96" -Value 0x00000020
+Set-ItemProperty -Path $cachePath -Name "OrbHeight.96" -Value 0x0000001e
+Set-ItemProperty -Path $cachePath -Name "IdealHeight.7" -Value 0x00000000
+Set-ItemProperty -Path $cachePath -Name "OrbWidth.144" -Value 0x00000030
+Set-ItemProperty -Path $cachePath -Name "OrbHeight.144" -Value 0x0000002e
 
 Stop-Process -Name "Explorer" -Force
 Write-Host "Configuring StartAllBack completed." -ForegroundColor Green
