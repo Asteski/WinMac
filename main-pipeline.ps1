43b1a9523d1e24955472980330bf3c0abd0cdace
Adams <adam.kamienski@accenture.com>
2024-03-19 23:40:21 +0100

    rm func ps1

--- main-pipeline.ps1
+++ main-pipeline.ps1
@@ -216,8 +216,19 @@ Start-Process explorer.exe
 Write-Host "Configuring StartAllBack completed." -ForegroundColor Green
 
 ## ! FIXME: Define ps subfolder in the project and use it to copy the function to the profile
-#
-# $funcPath = "$pwd\Utilities\func.ps1"
+# function WinMac {    
+#     $sab = (winget show --id StartIsBack.StartAllBack | Select-String -Pattern "Version").ToString().Split(":")[1].Trim()
+#     $pt = (winget show --id Microsoft.PowerToys | Select-String -Pattern "Version").ToString().Split(":")[1].Trim()
+#     $ev = (winget show --id Voidtools.Everything | Select-String -Pattern "Version").ToString().Split(":")[1].Trim()
+#     $output = @{
+#         'Version' = '0.0.1'
+#         'StartAllBackVersion' = $sab
+#         'PowerToysVersion' = $pt
+#         'EverythingVersion' = $ev
+#     }
+#     return $output
+# }
+# 
 # $funcContent = Get-Content -Path $funcPath -Raw
 # $filePath = Split-Path -Path $profile -Parent
 # $profilePath = Split-Path -Path $filePath -Parent
