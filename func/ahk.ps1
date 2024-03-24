ef4b42beffa6dd782fcb451bf0ebfd103a811be0
Adams <adam.kamienski@accenture.com>
2024-03-24 15:51:36 +0100

    ahk test

--- func/ahk.ps1
+++
@@ -1,7 +0,0 @@
-$ahkPath = Join-Path $pwd\bin "starthotkey.exe"
-if (-not (Test-Path $ahkPath)) {
-    Write-Host "starthotkey.exe not found at path: $exePath" -ForegroundColor Red
-    exit 1
-}
-$registryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
-Set-ItemProperty -Path $registryPath -Name "AutoHotKeyScript" -Value $exePath
