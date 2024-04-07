1d5f2d1ee384cd911d15554b78227fe4d0ca82e9
asteski <asteski@outlook.com>
2024-04-07 17:11:04 +0200

    ui test

--- install.ps1
+++ install.ps1
@@ -162,6 +162,26 @@ Set-ItemProperty -Path $RegPath -Name "ShowRecent" -Value 0 | Out-Null
 # Theme
 # TODO: change as a theme instead of a cursor
 Get-ChildItem .\config\cursor -Recurse -Filter "*inf" | ForEach-Object { PNPUtil.exe /add-driver $_.FullName /install }
+$RegConnect = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey([Microsoft.Win32.RegistryHive]”CurrentUser”,”$env:COMPUTERNAME”)
+$RegCursors = $RegConnect.OpenSubKey(“Control Panel\Cursors”,$true)
+$RegCursors.SetValue(“”,”Windows Black”)
+$RegCursors.SetValue(“AppStarting”,”%SystemRoot%\cursors\wait_r.cur”)
+$RegCursors.SetValue(“Arrow”,”%SystemRoot%\cursors\arrow_r.cur”)
+$RegCursors.SetValue(“Crosshair”,”%SystemRoot%\cursors\cross_r.cur”)
+$RegCursors.SetValue(“Hand”,””)
+$RegCursors.SetValue(“Help”,”%SystemRoot%\cursors\help_r.cur”)
+$RegCursors.SetValue(“IBeam”,”%SystemRoot%\cursors\beam_r.cur”)
+$RegCursors.SetValue(“No”,”%SystemRoot%\cursors\no_r.cur”)
+$RegCursors.SetValue(“NWPen”,”%SystemRoot%\cursors\pen_r.cur”)
+$RegCursors.SetValue(“SizeAll”,”%SystemRoot%\cursors\move_r.cur”)
+$RegCursors.SetValue(“SizeNESW”,”%SystemRoot%\cursors\size1_r.cur”)
+$RegCursors.SetValue(“SizeNS”,”%SystemRoot%\cursors\size4_r.cur”)
+$RegCursors.SetValue(“SizeNWSE”,”%SystemRoot%\cursors\size2_r.cur”)
+$RegCursors.SetValue(“SizeWE”,”%SystemRoot%\cursors\size3_r.cur”)
+$RegCursors.SetValue(“UpArrow”,”%SystemRoot%\cursors\up_r.cur”)
+$RegCursors.SetValue(“Wait”,”%SystemRoot%\cursors\busy_r.cur”)
+$RegCursors.Close()
+$RegConnect.Close()
 $CSharpSig = @'
 
 [DllImport(“user32.dll”, EntryPoint = “SystemParametersInfo”)]
