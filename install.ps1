c560821eacf6a038ad93ad2b89cf0bf89c3a0f7d
asteski <asteski@outlook.com>
2024-04-07 14:54:33 +0200

    dsc test

--- install.ps1
+++ install.ps1
@@ -43,7 +43,7 @@ Write-Information "Winget installation completed." -ForegroundColor Green
 
 Write-Host "Installing PowerToys..."  -ForegroundColor Yellow
 # $powerToystConfig = $pwd.Path +  ".\config\powertoys.dsc.yaml"
-winget configure .\config\powertoys.dsc.yaml
+winget configure .\config\powertoys.dsc.yaml --disable-interactivity
 Write-Host "Installing PowerToys completed." -ForegroundColor Green
 
 Write-Host "Installing Everything..."
