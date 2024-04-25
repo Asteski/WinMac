b47b7e99ef8d7b57c82e1e3ffd0a36b1fbb8ab91
asteski <asteski@outlook.com>
2024-04-26 00:42:59 +0200

    updated install.ps1

--- install.ps1
+++ install.ps1
@@ -115,7 +115,7 @@ Write-Host "Configuring PowerShell Profile..." -ForegroundColor Yellow
 
 $profilePath = $PROFILE | Split-Path | Split-Path
 $profileFile = $PROFILE | Split-Path -Leaf
-if ($promptSet -eq 'y') { $prompt = Get-Content "$pwd\config\terminal\winmac-prompt.ps1" -Raw }
+if ($promptSet -eq 'y') { $promptSet = Get-Content "$pwd\config\terminal\winmac-prompt.ps1" -Raw }
 else { $promptSet = Get-Content "$pwd\config\terminal\macos-prompt.ps1" -Raw }
 $functions = Get-Content "$pwd\config\terminal\functions.ps1" -Raw
 
