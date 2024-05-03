2af2da72eb1e262e567f9f1fc1ebc69e25bff0a3
asteski <asteski@outlook.com>
2024-05-04 00:03:05 +0200

    custom install test

--- config/terminal/macos-prompt.ps1
+++ config/terminal/macos-prompt.ps1
@@ -1,9 +1,16 @@
 
 # MacOS prompt
+function Test-Admin
+{
+    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)  
+}
+
 function prompt {
     $userName = $env:USERNAME
     $computerName = hostname
     $folder = Split-Path -Leaf $pwd
+    if (Test-Admin -eq $true) { $userName = "$([char]27)[91m$($userName)$([char]27)[0m" } else { $userName = "$([char]27)[93m$($userName)$([char]27)[0m" }
     if ($folder -eq $env:USERNAME)
     {
         $userName + '@' + $computerName + ' ~ % '
--- config/terminal/winmac-prompt.ps1
+++ config/terminal/winmac-prompt.ps1
@@ -1,9 +1,16 @@
 
 # WinMac prompt
+function Test-Admin
+{
+    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)  
+}
+
 function prompt {
     $userName = $env:USERNAME
     $folder = Split-Path -Leaf $pwd
     $date = Get-Date -f HH:mm:ss
+    if (Test-Admin -eq $true) { $userName = "$([char]27)[91m$($userName)$([char]27)[0m" } else { $userName = "$([char]27)[93m$($userName)$([char]27)[0m" }
     if ($folder -eq $env:USERNAME)
     {
         "$([char]27)[92m$($date)$([char]27)[0m" + ' ' + $userName + ' @ ~' + "$([char]27)[93m$(' > ')$([char]27)[0m"
