9ac28fc3fcc0045a1aa5f232ddcd526674560e06
asteski <asteski@outlook.com>
2024-05-14 11:21:28 +0200

    dock update

--- uninstall.ps1
+++ uninstall.ps1
@@ -54,7 +54,7 @@ $([char]27)[93m$("Please select options you want to uninstall:")$([char]27)[0m
     Write-Host "5. Open-Shell"
     Write-Host "6. TopNotify"
     Write-Host "7. Nexus Dock"
-    Write-Host "8. Other (cursor, pinned folders, shortcut arrows, recycle bin desktop icon)"
+    Write-Host "8. Other (cursor, pinned folders, shortcut arrows, add recycle bin desktop icon)"
     Write-Host
     $selection = Read-Host "Enter the numbers of options you want to uninstall (separated by commas)"
     $selectedApps = @()
