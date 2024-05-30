5d9a2c720047ba9d5dc524005ce96e6be6a703c2
asteski <asteski@outlook.com>
2024-05-31 00:07:22 +0200

    updated stacks

--- stahky.ps1
+++ stahky.ps1
@@ -1,29 +1,20 @@
 $url = "https://github.com/joedf/stahky/releases/download/v0.1.0.8/stahky_U64_v0.1.0.8.zip"
-$outputPath = "$($pwd)\stahky_U64_v0.1.0.8.zip"
-$exePath = ($env:AppData | Split-Path) + "\local\Stahky"
+$outputPath = "$pwd\stahky_U64_v0.1.0.8.zip"
+$exePath = "$env:programfiles\Stahky"
 
 Invoke-WebRequest -Uri $url -OutFile $outputPath
 Expand-Archive -Path $outputPath -DestinationPath $exePath
 
-# $shell = New-Object -ComObject Shell.Application
-# $shortcutPath1 = "$($pwd)\config\taskbar\stacks\shortcuts\Control.stahky.lnk"
-# $shortcutPath2 = "$($pwd)\config\taskbar\stacks\shortcuts\Favorites.stahky.lnk"
+$shell = New-Object -ComObject WScript.Shell
+$shortcutPath = "$pwd\stahky.lnk"
+$targetPath = "$pwd\stahky.exe /stahky $pwd\config\taskbar\stacks\shortcuts\Favorites.stahky.lnk"
 
-# $taskbarPath = [System.IO.Path]::Combine($env:APPDATA, "Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar")
-# $taskbarFolder = $shell.Namespace($taskbarPath)
-
-# $shortcut1 = $shell.CreateShortcut($shortcutPath1)
-# $shortcut2 = $shell.CreateShortcut($shortcutPath2)
-
-# $taskbarFolder.CopyHere($shortcut1.FullName)
-# $taskbarFolder.CopyHere($shortcut2.FullName)
-
-$shortcutPath = "$($pwd)\config\taskbar\stacks\shortcuts\Favorites.stahky.lnk"
 $shortcut = $shell.CreateShortcut($shortcutPath)
-$shortcut.TargetPath = "$($exePath)\stahky.exe /stahky $($pwd)\config\taskbar\stacks\shortcuts\Favorites.stahky.lnk"
+$shortcut.TargetPath = $targetPath
 $shortcut.Save()
 
-$Target = "$($pwd)\config\taskbar\stacks\shortcuts\Favorites.stahky.lnk"
+
+$Target = "$pwd\config\taskbar\stacks\shortcuts\Favorites.stahky.lnk"
 $KeyPath1  = "HKCU:\SOFTWARE\Classes"
 $KeyPath2  = "*"
 $KeyPath3  = "shell"
