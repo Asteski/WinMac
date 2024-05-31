a06c35f238469128d451f6b8226ddb485c47ce38
asteski <asteski@outlook.com>
2024-05-31 18:38:54 +0200

    updated install.ps1

--- install.ps1
+++ install.ps1
@@ -123,10 +123,13 @@ else
 
 $lightOrDark = Read-Host "Enter 'L' for light themed or 'D' for dark themed Windows"
 if ($lightOrDark -eq "L" -or $lightOrDark -eq "l") {
+    $stackTheme = 'light'
     Write-Host "Using light theme." -ForegroundColor Yellow 
 } elseif ($lightOrDark -eq "D" -or $lightOrDark -eq "d") {
+    $stackTheme = 'dark'
     Write-Host "Using dark theme." -ForegroundColor Yellow
 } else {
+    $stackTheme = 'light'
     Write-Host "Invalid input. Defaulting to light theme." -ForegroundColor Yellow
 }
 
@@ -399,9 +402,20 @@ foreach ($app in $selectedApps) {
             Write-Host "Configuring TopNotify completed." -ForegroundColor Green
         }
         "7" {
-            ...
-            ...
-            ...
+            $url = "https://github.com/joedf/stahky/releases/download/v0.1.0.8/stahky_U64_v0.1.0.8.zip"
+            $outputPath = "$pwd\stahky_U64_v0.1.0.8.zip"
+            $exePath = "$env:PROGRAMFILES\Stahky"
+            
+            New-Item -ItemType Directory -Path $env:PROGRAMFILES\Stahky -Force | Out-Null
+            New-Item -ItemType Directory -Path $env:PROGRAMFILES\Stahky\config -Force | Out-Null
+            Invoke-WebRequest -Uri $url -OutFile $outputPath
+            if (Test-Path -Path "$exePath\stahky.exe") {
+                Write-Host "Stahky already exists."
+            } else {
+                Expand-Archive -Path $outputPath -DestinationPath $exePath
+            }
+            Copy-Item -Path $pwd\config\taskbar\stacks\* -Destination $exePath\config -Recurse -Force
+            Copy-Item -Path $exePath\config\themes\stahky-$stackTheme.ini -Destination $exePath\stahky.init
         }
         "8" {
             # Other
