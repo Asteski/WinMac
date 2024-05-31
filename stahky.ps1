ccd78562e3d89ad3f675688389f8acc41207201a
asteski <asteski@outlook.com>
2024-05-31 16:05:02 +0200

    updated stacks

--- stahky.ps1
+++ stahky.ps1
@@ -2,8 +2,8 @@ $url = "https://github.com/joedf/stahky/releases/download/v0.1.0.8/stahky_U64_v0
 $outputPath = "$pwd\stahky_U64_v0.1.0.8.zip"
 $exePath = "$env:PROGRAMFILES\Stahky"
 
-New-Item -ItemType Directory -Path $env:PROGRAMFILES\Stahky -Force
-New-Item -ItemType Directory -Path $env:PROGRAMFILES\Stahky\config -Force
+New-Item -ItemType Directory -Path $env:PROGRAMFILES\Stahky -Force | Out-Null
+New-Item -ItemType Directory -Path $env:PROGRAMFILES\Stahky\config -Force | Out-Null
 Invoke-WebRequest -Uri $url -OutFile $outputPath
 Expand-Archive -Path $outputPath -DestinationPath $exePath
 Copy-Item -Path $pwd\config\taskbar\stacks\* -Destination $exePath\config -Recurse -Force
