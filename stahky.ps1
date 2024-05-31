ec739df91e3cfa2058e21cca4d3498a91b54f5d1
asteski <asteski@outlook.com>
2024-05-31 17:00:14 +0200

    updated stacks

--- stahky.ps1
+++ stahky.ps1
@@ -1,6 +1,7 @@
 $url = "https://github.com/joedf/stahky/releases/download/v0.1.0.8/stahky_U64_v0.1.0.8.zip"
 $outputPath = "$pwd\stahky_U64_v0.1.0.8.zip"
 $exePath = "$env:PROGRAMFILES\Stahky"
+# $shortcutPath = "$exePath\config\shortcuts\control.stahky.lnk"
 
 New-Item -ItemType Directory -Path $env:PROGRAMFILES\Stahky -Force | Out-Null
 New-Item -ItemType Directory -Path $env:PROGRAMFILES\Stahky\config -Force | Out-Null
@@ -9,7 +10,7 @@ Expand-Archive -Path $outputPath -DestinationPath $exePath
 Copy-Item -Path $pwd\config\taskbar\stacks\* -Destination $exePath\config -Recurse -Force
 Copy-Item -Path $exePath\config\themes\stahky-dark.ini -Destination $exePath\stahky.init
 
-$shortcutPath = "$exePath\control.stahky.lnk"
+$shortcutPath = "$exePath\config\shortcuts\control.stahky.lnk"
 $shell = New-Object -ComObject WScript.Shell
 $taskbarPath = [System.IO.Path]::Combine($env:APPDATA, "Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar")
 $shortcut = $shell.CreateShortcut($shortcutPath)
