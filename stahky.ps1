cfd6cfc9511c162b8115b6e0d998e29cb76742ca
asteski <asteski@outlook.com>
2024-05-31 17:15:47 +0200

    updated stacks

--- stahky.ps1
+++ stahky.ps1
@@ -6,11 +6,15 @@ $exePath = "$env:PROGRAMFILES\Stahky"
 New-Item -ItemType Directory -Path $env:PROGRAMFILES\Stahky -Force | Out-Null
 New-Item -ItemType Directory -Path $env:PROGRAMFILES\Stahky\config -Force | Out-Null
 Invoke-WebRequest -Uri $url -OutFile $outputPath
-Expand-Archive -Path $outputPath -DestinationPath $exePath
+if (Test-Path -Path "$exePath\stahky.exe") {
+    Write-Host "Stahky already exists."
+} else {
+    Expand-Archive -Path $outputPath -DestinationPath $exePath
+}
 Copy-Item -Path $pwd\config\taskbar\stacks\* -Destination $exePath\config -Recurse -Force
 Copy-Item -Path $exePath\config\themes\stahky-dark.ini -Destination $exePath\stahky.init
 
-$shortcutPath = "$exePath\config\shortcuts\control.stahky.lnk"
+$shortcutPath = "$exePath\config\shortcuts\Control.stahky.lnk"
 $shell = New-Object -ComObject WScript.Shell
 $taskbarPath = [System.IO.Path]::Combine($env:APPDATA, "Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar")
 $shortcut = $shell.CreateShortcut($shortcutPath)
