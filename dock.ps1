897abcb16474983e751de595e0e9ab4860bb8308
asteski <asteski@outlook.com>
2024-06-02 01:44:52 +0200

    updated dock.ps1

--- dock.ps1
+++ dock.ps1
@@ -83,7 +83,7 @@ elseif (($roundedOrSquared -ne "S" -or $roundedOrSquared -ne "s") -and ($lightOr
 }
 
 reg import $regFile
-Remove-Item -Path "Registry::HKEY_CURRENT_USER\Software\WinSTEP2000\NeXuS\Docks" -Name "DockLabelColorHotTrack1"
+Remove-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Software\WinSTEP2000\NeXuS\Docks" -Name "DockLabelColorHotTrack1"
 Start-Sleep 2
 Write-Host "Configuring Nexus Dock completed." -ForegroundColor Green
 
