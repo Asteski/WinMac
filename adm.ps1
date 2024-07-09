42b1f0f422614038e92313eb13b0472539852bbb
asteski <asteski@outlook.com>
2024-07-09 18:12:35 +0200

    dir var fix

--- adm.ps1
+++ adm.ps1
@@ -4,5 +4,5 @@ $newContent = $fileContent -replace "<STRING_TO_REPLACE>", "C:\Users\$ENV:USERNA
 New-Item -ItemType File -Path ".\temp\scripts.yaml" -Force
 $newContent | Set-Content -Path ".\temp\scripts.yaml"
 #Get-Process "AutoDarkMode" -ErrorAction SilentlyContinue | Stop-Process -Force
-Copy-Item ".\temp\scripts.yaml" "C:\Users\$ENV:USERNAME\AppData\Roaming\AutoDarkMode"  -Force
-Copy-Item ".\config\adm\color-mode.ps1" "C:\Users\$ENV:USERNAME\AppData\Roaming\AutoDarkMode" -Force
+Copy-Item ".\temp\scripts.yaml" "C:\Users\$ENV:USERNAME\AppData\Roaming\AutoDarkMode\scripts.yaml"  -Force
+Copy-Item ".\config\adm\color-mode.ps1" "C:\Users\$ENV:USERNAME\AppData\Roaming\AutoDarkMode\color-mode.ps1" -Force
