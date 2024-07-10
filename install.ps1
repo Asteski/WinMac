853c436ef8f5d3f5d714c79bad5a07d2fc9627ab
asteski <asteski@outlook.com>
2024-07-10 20:42:04 +0200

    updated TopNotify version

--- install.ps1
+++ install.ps1
@@ -414,9 +414,9 @@ foreach ($app in $selectedApps) {
         "6" {
             # TopNotify
             Write-Host "Configuring TopNotify..." -ForegroundColor Yellow
-            Invoke-WebRequest "https://github.com/SamsidParty/TopNotify/releases/download/2.2.0/TopNotify.zip" -OutFile TopNotify.zip
+            Invoke-WebRequest "https://github.com/SamsidParty/TopNotify/releases/download/2.3.7/Install_TopNotify.exe" -OutFile "Install_TopNotify.exe"
             $exePath = ($env:AppData | Split-Path) + "\local\TopNotify"
-            Expand-Archive TopNotify.zip -DestinationPath $exePath -Force
+            # Expand-Archive TopNotify.zip -DestinationPath $exePath -Force
             Start-Process -FilePath $exePath\TopNotify.exe
             Remove-Item -Path TopNotify.zip -Force
             Write-Host "Configuring TopNotify completed." -ForegroundColor Green
