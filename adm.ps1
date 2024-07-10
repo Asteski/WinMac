31e064d0a7d02c6a1a4f3a8a3c17353e3cd5ad63
asteski <asteski@outlook.com>
2024-07-10 12:32:05 +0200

    updated adm.ps1

--- adm.ps1
+++ adm.ps1
@@ -1,11 +1,18 @@
+echo 0
 winget install --id Armin2208.WindowsAutoNightMode --silent
-Start-Process -FilePath "C:\Users\$ENV:USERNAME\AppData\Local\Programs\AutoDarkMode\adm-app\autodarkmodesvc.exe" -Verb RunAs
+echo 1
+Start-Process -FilePath "C:\Users\$ENV:USERNAME\AppData\Local\Programs\AutoDarkMode\adm-app\autodarkmodeapp.exe" -Verb RunAs
+echo 2
 Start-Sleep 10
+echo 3
 Stop-Process -Name "AutoDarkMode*" -Force
+echo 4
 $fileContent = Get-Content -Path ".\config\adm\scripts.yaml"
 $newContent = $fileContent -replace "<STRING_TO_REPLACE>", "C:\Users\$ENV:USERNAME\AppData\Roaming\AutoDarkMode"
 New-Item -ItemType File -Path ".\temp\scripts.yaml" -Force
 $newContent | Set-Content -Path ".\temp\scripts.yaml"
 Copy-Item ".\temp\scripts.yaml" "C:\Users\$ENV:USERNAME\AppData\Roaming\AutoDarkMode\scripts.yaml" -Force
 Copy-Item ".\config\adm\color-mode.ps1" "C:\Users\$ENV:USERNAME\AppData\Roaming\AutoDarkMode\color-mode.ps1" -Force
-Start-Process -FilePath "C:\Users\$ENV:USERNAME\AppData\Local\Programs\AutoDarkMode\adm-app\autodarkmodesvc.exe" -Verb RunAs
+echo 5
+Start-Process -FilePath "C:\Users\$ENV:USERNAME\AppData\Local\Programs\AutoDarkMode\adm-app\autodarkmodeapp.exe" -Verb RunAs
+echo 6
