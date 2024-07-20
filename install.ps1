761fdf7641b81584177a9a70187d6d0fa9bb76ad
asteski <asteski@outlook.com>
2024-07-20 14:19:20 +0200

    init push

--- install.ps1
+++ install.ps1
@@ -290,14 +290,14 @@ foreach ($app in $selectedApps) {
         }
         "4" {
             ## StartAllBack
-            $taskbarHandle = [Taskbar]::FindWindow("Shell_TrayWnd", "") | Out-Null
-            $HWND_TOP = [IntPtr]::Zero
-            $SWP_SHOWWINDOW = 0x0040
-            [Taskbar]::SetWindowPos($taskbarHandle, $HWND_TOP, 0, 0, 0, 0, $SWP_SHOWWINDOW) | Out-Null
+            $sabLocal = ($env:AppData | Split-Path) + "\local\StartAllBack\Orbs"
             $sabRegPath = "HKCU:\Software\StartIsBack"
+            $registryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\StuckRectsLegacy"
+            $registryValueName = "Settings"
+            $registryValueData = @(0x30,0x00,0x00,0x00,0xfe,0xff,0xff,0xff,0x02,0x00,0x00,0x00,0x01,0x00,0x00,0x00,0x5a,0x00,0x00,0x00,0x32,0x00,0x00,0x00,0x26,0x07,0x00,0x00,0x00,0x00,0x00,0x00,0x80,0x07,0x00,0x00,0x38,0x04,0x00,0x00,0x78,0x00,0x00,0x00,0x01,0x00,0x00,0x00)
+            Set-ItemProperty -Path $registryPath -Name $registryValueName -Value $registryValueData
             Write-Host "Configuring StartAllBack..." -ForegroundColor Yellow
             winget install --id "StartIsBack.StartAllBack" --source winget --silent | Out-Null
-            $sabLocal = ($env:AppData | Split-Path) + "\local\StartAllBack\Orbs"
             Copy-Item $pwd\config\taskbar\orbs\* $sabLocal -Force | Out-Null
             Set-ItemProperty -Path $exRegPath\HideDesktopIcons\NewStartPanel -Name "{645FF040-5081-101B-9F08-00AA002F954E}" -Value 1
             Set-ItemProperty -Path $exRegPath\Advanced -Name "TaskbarSizeMove" -Value 1
@@ -338,26 +338,26 @@ foreach ($app in $selectedApps) {
             else { Set-ItemProperty -Path $sabRegPath\DarkMagic -Name "Unround" -Value 1 }
             Stop-Process -Name Explorer -Force
             Start-Sleep 5
-            $screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
-            $w = $screen.width/4
-            [Windows.Forms.Cursor]::Position = "$($w),$($screen.Height)"
-            Start-Sleep -Seconds 2
-            [MouseInput]::HoldLeftMouseButton()
-            Start-Sleep -Seconds 2
-            [Windows.Forms.Cursor]::Position = "$($w),1"
-            Start-Sleep -Seconds 2
-            [MouseInput]::ReleaseLeftMouseButton()
-            Start-Sleep -Seconds 2
-            $screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
-            $centerX = $screen.Width / 2
-            $centerY = $screen.Height / 2
-            [Windows.Forms.Cursor]::Position = "$centerX,$centerY"
-            Start-Sleep -Milliseconds 100
-            [MouseInput]::HoldLeftMouseButton()
-            Start-Sleep -Milliseconds 100
-            [MouseInput]::ReleaseLeftMouseButton()
-            Start-Sleep -Milliseconds 100
-            Start-Sleep -Seconds 2
+            # $screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
+            # $w = $screen.width/4
+            # [Windows.Forms.Cursor]::Position = "$($w),$($screen.Height)"
+            # Start-Sleep -Seconds 2
+            # [MouseInput]::HoldLeftMouseButton()
+            # Start-Sleep -Seconds 2
+            # [Windows.Forms.Cursor]::Position = "$($w),1"
+            # Start-Sleep -Seconds 2
+            # [MouseInput]::ReleaseLeftMouseButton()
+            # Start-Sleep -Seconds 2
+            # $screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
+            # $centerX = $screen.Width / 2
+            # $centerY = $screen.Height / 2
+            # [Windows.Forms.Cursor]::Position = "$centerX,$centerY"
+            # Start-Sleep -Milliseconds 100
+            # [MouseInput]::HoldLeftMouseButton()
+            # Start-Sleep -Milliseconds 100
+            # [MouseInput]::ReleaseLeftMouseButton()
+            # Start-Sleep -Milliseconds 100
+            # Start-Sleep -Seconds 2
             Write-Host "Configuring StartAllBack completed." -ForegroundColor Green
         }
         "5" {
