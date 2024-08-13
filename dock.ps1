b911fa6469649d14911518b98180da028b7df0ea
asteski <asteski@outlook.com>
2024-08-13 21:32:50 +0200

    updated dock.ps1

--- dock.ps1
+++ dock.ps1
@@ -113,11 +113,9 @@ reg import $regFile > $null 2>&1
 Remove-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Software\WinSTEP2000\NeXuS\Docks" -Name "DockLabelColorHotTrack1" -ErrorAction SilentlyContinue # | Out-Null
 Remove-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Software\WinSTEP2000\NeXuS\Docks" -Name "1Type6" -ErrorAction SilentlyContinue # | Out-Null
 Set-ItemProperty -Path "HKCU:\Software\WinSTEP2000\NeXuS\Docks" -Name "1Path6" -Value $downloadsPath
-Remove-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Software\WinSTEP2000\NeXuS\Docks" -Name "1Type6" -ErrorAction SilentlyContinue # | Out-Null
 Start-Process 'C:\Program Files (x86)\Winstep\Nexus.exe' # | Out-Null
 while (!(Get-Process nexus -ErrorAction SilentlyContinue)) { Start-Sleep 1 }
 Write-Host "Nexus Dock installation completed.`n" -ForegroundColor Green
-
 Write-Host "Clean up..." -ForegroundColor Yellow
 $programsDir = "$($env:APPDATA)\Microsoft\Windows\Start Menu\Programs"
 Move-Item -Path "C:\Users\$env:USERNAME\Desktop\Nexus.lnk" -Destination $programsDir -Force -ErrorAction SilentlyContinue # | Out-Null
