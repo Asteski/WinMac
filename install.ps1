0ca52f81d11f9cd9b718f8db04542b46442b9300
asteski <asteski@outlook.com>
2024-08-29 23:49:57 +0200

    updated install.ps1

--- install.ps1
+++ install.ps1
@@ -448,8 +448,16 @@ foreach ($app in $selectedApps) {
             New-Item -ItemType Directory -Path "$env:LOCALAPPDATA\WinMac\" | Out-Null
             Copy-Item .\bin\$fileName "$env:LOCALAPPDATA\WinMac\" | Out-Null
             if (-not $adminTest) {
-                $principal = New-ScheduledTaskPrincipal -UserId "LOCALSERVICE" -LogonType ServiceAccount
-                Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -TaskPath $taskFolder -Settings $settings # -ErrorAction SilentlyContinue | Out-Null
+                $logonUser = $env:UserName
+                $logonDomain = $env:UserDomain
+                schtasks.exe /create /tn $taskName `
+                /tr "`"$fileDirectory\$fileName`"" `
+                /sc onlogon `
+                /ru "$logonDomain\$logonUser" `
+                /f
+                Write-Host "Task '$taskName' registered successfully."
+                # $principal = New-ScheduledTaskPrincipal -UserId "LOCALSERVICE" -LogonType ServiceAccount
+                # Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -TaskPath $taskFolder -Settings $settings # -ErrorAction SilentlyContinue | Out-Null
             } else {
                 $principal = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Administrators" -RunLevel Highest
                 Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Principal $principal -TaskPath $taskFolder -Settings $settings # -ErrorAction SilentlyContinue | Out-Null
