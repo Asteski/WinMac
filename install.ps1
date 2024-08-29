1453e2775eca2cc50caa9df47c229f1ff6b29235
asteski <asteski@outlook.com>
2024-08-30 00:15:44 +0200

    updated install.ps1

--- install.ps1
+++ install.ps1
@@ -470,6 +470,111 @@ foreach ($app in $selectedApps) {
             Write-Host "WinMac Keybindings installation completed." -ForegroundColor Green
         }
         "10" {
+            ## Nexus Dock
+            if ($adminTest) {
+                Write-Host "`nWinstep Nexus requires non-administrator privileges. Please run the script as a standard user.`n" -ForegroundColor Red
+            }
+            else {
+                Write-Host "`nInstalling Nexus Dock...`n" -ForegroundColor Yellow
+                $roundedOrSquared = Read-Host "Enter 'R' for rounded dock or 'S' for squared dock"
+                if ($roundedOrSquared -eq "R" -or $roundedOrSquared -eq "r") {
+                    Write-Host "Using rounded dock.`n" -ForegroundColor Green 
+                } elseif ($roundedOrSquared -eq "S" -or $roundedOrSquared -eq "s") {
+                    Write-Host "Using squared dock.`n" -ForegroundColor Green
+                } else {
+                    Write-Host "Invalid input. Defaulting to rounded dock.`n" -ForegroundColor Yellow
+                }
+
+                $lightOrDark = Read-Host "Enter 'L' for light themed dock or 'D' for dark themed dock"
+                if ($lightOrDark -eq "L" -or $lightOrDark -eq "l") {
+                    Write-Host "Using light theme.`n" -ForegroundColor Green
+                } elseif ($lightOrDark -eq "D" -or $lightOrDark -eq "d") {
+                    Write-Host "Using dark theme.`n" -ForegroundColor Green
+                } else {
+                    Write-Host "Invalid input. Defaulting to light theme.`n" -ForegroundColor Yellow
+                }
+                Start-Sleep 1
+                Write-Host "`Installing Nexus Dock...`n" -ForegroundColor Yellow
+                $downloadUrl = "https://www.winstep.net/nexus.zip"
+                $downloadPath = "dock.zip"
+                if (-not (Test-Path $downloadPath)) {
+                    Invoke-WebRequest -Uri $downloadUrl -OutFile $downloadPath
+                }
+                Expand-Archive -Path $downloadPath -DestinationPath $pwd -Force
+                Start-Process -FilePath ".\NexusSetup.exe" -ArgumentList "/silent"
+                Start-Sleep 10
+                $process1 = Get-Process -Name "NexusSetup" -ErrorAction SilentlyContinue
+                while ($process1) {
+                    Start-Sleep 5
+                    $process1 = Get-Process -Name "NexusSetup" -ErrorAction SilentlyContinue
+                }
+                Start-Sleep 10
+                $process2 = Get-Process -Name "Nexus" -ErrorAction SilentlyContinue
+                if (!($process2)) {
+                    Start-Sleep 5
+                    $process2 = Get-Process -Name "Nexus" -ErrorAction SilentlyContinue
+                } else { Start-Sleep 10 }
+                Get-Process -n Nexus | Stop-Process
+                $winStep = 'C:\Users\Public\Documents\WinStep'
+                Remove-Item -Path "$winStep\Themes\*" -Recurse -Force | Out-Null
+                Copy-Item -Path "config\dock\themes\*" -Destination "$winStep\Themes\" -Recurse -Force | Out-Null
+                Remove-Item -Path "$winStep\NeXus\Indicators\*" -Force -Recurse | Out-Null
+                Copy-Item -Path "config\dock\indicators\*" -Destination "$winStep\NeXus\Indicators\" -Recurse -Force | Out-Null
+                New-Item -ItemType Directory -Path "$winStep\Sounds" -Force | Out-Null
+                Copy-Item -Path "config\dock\sounds\*" -Destination "$winStep\Sounds\" -Recurse -Force | Out-Null
+                New-Item -ItemType Directory -Path "$winStep\Icons" -Force | Out-Null
+                Copy-Item config\dock\icons "$winStep" -Recurse -Force | Out-Null
+                $regFile = "$pwd\config\dock\winstep.reg"
+                $tempFolder = "$pwd\temp"
+                $downloadsPath = "$env:USERPROFILE\Downloads"
+                if (-not (Test-Path $tempFolder)) {
+                    New-Item -ItemType Directory -Path $tempFolder -Force | Out-Null
+                }
+                if ($roundedOrSquared -eq "S" -or $roundedOrSquared -eq "s") {
+                    $modifiedContent = Get-Content $regFile | ForEach-Object { $_ -replace "Rounded", "Squared" }
+                    $modifiedFile = "$pwd\temp\winstep.reg"
+                    $modifiedContent | Out-File -FilePath $modifiedFile -Encoding UTF8 | Out-Null
+                    $regFile = $modifiedFile
+                    if ($lightOrDark -eq "D" -or $lightOrDark -eq "d") {
+                        $modifiedContent = Get-Content $regFile | ForEach-Object { $_ -replace "Light", "Dark" }
+                        $modifiedContent = $modifiedContent | ForEach-Object { $_ -replace '"UIDarkMode"="3"', '"UIDarkMode"="1"' }
+                        $modifiedContent = $modifiedContent | ForEach-Object { $_ -replace "1644825", "15658734" }
+                        $modifiedContent = $modifiedContent | ForEach-Object { $_ -replace "16119283", "2563870" }
+                        $modifiedFile = "$pwd\temp\winstep.reg"
+                        $modifiedContent | Out-File -FilePath $modifiedFile -Encoding UTF8 | Out-Null
+                    }
+                }
+                elseif (($roundedOrSquared -ne "S" -or $roundedOrSquared -ne "s") -and ($lightOrDark -eq "D" -or $lightOrDark -eq "d")) {
+                    $modifiedContent = Get-Content $regFile | ForEach-Object { $_ -replace "Light", "Dark" }
+                    $modifiedContent = $modifiedContent | ForEach-Object { $_ -replace '"UIDarkMode"="3"', '"UIDarkMode"="1"' }
+                    $modifiedContent = $modifiedContent | ForEach-Object { $_ -replace "1644825", "15658734" }
+                    $modifiedContent = $modifiedContent | ForEach-Object { $_ -replace "16119283", "2563870" }
+                    $modifiedFile = "$pwd\temp\winstep.reg"
+                    $modifiedContent | Out-File -FilePath $modifiedFile -Encoding UTF8 | Out-Null
+                    $regFile = $modifiedFile
+                }
+                reg import $regFile > $null 2>&1
+                Remove-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Software\WinSTEP2000\NeXuS\Docks" -Name "DockLabelColorHotTrack1" -ErrorAction SilentlyContinue | Out-Null
+                Remove-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Software\WinSTEP2000\NeXuS\Docks" -Name "1Type6" -ErrorAction SilentlyContinue | Out-Null
+                Remove-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Software\WinSTEP2000\NeXuS\Docks" -Name "1Type7" -ErrorAction SilentlyContinue | Out-Null
+                Set-ItemProperty -Path "HKCU:\Software\WinSTEP2000\NeXuS\Docks" -Name "1Path6" -Value $downloadsPath
+                Set-ItemProperty -Path "HKCU:\Software\WinSTEP2000\NeXuS\Docks" -Name "1Path7" -Value "$env:APPDATA\Microsoft\Windows\Recent\"
+                Start-Process 'C:\Program Files (x86)\Winstep\Nexus.exe' | Out-Null
+                while (!(Get-Process nexus -ErrorAction SilentlyContinue)) { Start-Sleep 1 }
+                Write-Host "Nexus Dock installation completed.`n" -ForegroundColor Green
+                Write-Host "Clean up..." -ForegroundColor Yellow
+                $programsDir = "$($env:APPDATA)\Microsoft\Windows\Start Menu\Programs"
+                Move-Item -Path "C:\Users\$env:USERNAME\Desktop\Nexus.lnk" -Destination $programsDir -Force -ErrorAction SilentlyContinue | Out-Null
+                Move-Item -Path "C:\Users\$env:USERNAME\OneDrive\Desktop\Nexus.lnk" -Destination $programsDir -Force -ErrorAction SilentlyContinue | Out-Null
+                Remove-Item "$pwd\temp\*" -Recurse -Force -ErrorAction SilentlyContinue | Out-Null
+                Remove-Item .\dock.zip -Force | Out-Null
+                Remove-Item .\ReadMe.txt -Force | Out-Null
+                Remove-Item .\NexusSetup.exe -Force | Out-Null
+                Write-Host "Nexus Dock installation completed." -ForegroundColor Green
+            }
+
+        }
+        "11" {
             # Other
             ## Black Cursor
             Write-Host "Configuring black cursor..." -ForegroundColor Yellow
