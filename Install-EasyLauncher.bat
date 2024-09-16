bc8403bdd2e8c9717fbcba6fcf1efb9ca4d8036c
asteski <asteski@outlook.com>
2024-09-17 00:32:48 +0200

    updated winmac menu

--- Install-EasyLauncher.bat
+++ Install-EasyLauncher.bat
@@ -1,8 +1,2 @@
 @echo off
-:: Lines starting with two colons :: are comments
-
-:: Sets the execution policy for the current process to allow the script to run, then runs it
 pwsh -NoProfile -ExecutionPolicy Bypass -File "InstallGUI.ps1"
-
-:: Alternatively, you could run this command yourself in a powershell window before running the script:   		Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
-:: Which also would just allow scripts for that current PowerShell session, then you can run the script with: 	.\Super_God_Mode.ps1
