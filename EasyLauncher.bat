db8309b6681451f6c3c6a86e82c3ed49a266969b
asteski <asteski@outlook.com>
2024-09-24 17:53:45 +0200

    test installGUI.ps1

--- EasyLauncher.bat
+++ EasyLauncher.bat
@@ -1,32 +1,13 @@
 @echo off
 cd /d "%~dp0\pwsh"
-
-:: Debugging: Check what argument is passed
-echo Argument passed: %1
-
-:: Check if no argument is passed
 if "%1"=="" (
     pwsh -NoProfile -ExecutionPolicy Bypass -File "installGUI.ps1"
-    goto :eof
-)
-
-:: Check if -uninstall is passed
-if /i "%1"=="-uninstall" (
+) else if /i "%1"=="-uninstall" (
     pwsh -NoProfile -ExecutionPolicy Bypass -File "uninstallGUI.ps1"
-    goto :eof
+) else if /i "%1"=="-nogui" (
+    pwsh -NoProfile -ExecutionPolicy Bypass -File "installGUI.ps1" -nogui
+) else if /i "%1"=="-uninstallnogui" (
+    pwsh -NoProfile -ExecutionPolicy Bypass -File "uninstall.ps1" -nogui
+) else (
+    echo [31mInvalid argument.[0m
 )
-
-:: Check if -nogui is passed
-if /i "%1"=="-nogui" (
-    pwsh -NoProfile -ExecutionPolicy Bypass -File "installGUI.ps1" -ArgumentList '-nogui'
-    goto :eof
-)
-
-:: Check if -uninstallnogui is passed
-if /i "%1"=="-uninstallnogui" (
-    pwsh -NoProfile -ExecutionPolicy Bypass -File "uninstall.ps1" -ArgumentList '-nogui'
-    goto :eof
-)
-
-:: Handle invalid argument case
-echo [31mInvalid argument.[0m
