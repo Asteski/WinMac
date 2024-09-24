f520d5732b7a9ee12bfcfff0fd0b062972fa563d
asteski <asteski@outlook.com>
2024-09-24 17:07:30 +0200

    test installGUI.ps1

--- EasyLauncher.bat
+++ EasyLauncher.bat
@@ -5,9 +5,9 @@ if "%1"=="" (
 ) else if "%1"=="-uninstall" (
     pwsh -NoProfile -ExecutionPolicy Bypass -File "uninstallGUI.ps1"
 ) else if "%1"=="-nogui" (
-    pwsh -NoProfile -ExecutionPolicy Bypass -File "install.ps1 -nogui"
+    pwsh -NoProfile -ExecutionPolicy Bypass -File "installGUI.ps1" -ArgumentList "-nogui"
 ) else if "%1"=="-uninstallnogui" (
-    pwsh -NoProfile -ExecutionPolicy Bypass -File "uninstall.ps1 -nogui"
+    pwsh -NoProfile -ExecutionPolicy Bypass -File "uninstall.ps1  -ArgumentList "-nogui"
 ) else (
     echo [31mInvalid argument.[0m
 )
