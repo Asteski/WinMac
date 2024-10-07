18ce12982a69914136d148f35f786cc5ee8e09d9
asteski <asteski@outlook.com>
2024-10-07 21:27:57 +0200

    fixed install.ps1

--- pwsh/install.ps1
+++ pwsh/install.ps1
@@ -503,6 +503,7 @@ foreach ($app in $selectedApps) {
     # PowerToys
         "1" {
             Write-Host "Installing PowerToys..." -ForegroundColor Yellow
+            winget configure --enable | Out-Null
             winget configure ..\config\powertoys.dsc.yaml --accept-configuration-agreements | Out-Null
             Write-Host "PowerToys installation completed." -ForegroundColor Green
         }
