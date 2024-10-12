1f3e6801d3d1aa5e0220eee1900cc88f6561d5d0
akamienski <kamienski.adam@gmail.com>
2024-10-12 17:37:15 +0200

    updated git-profile.ps1

--- config/terminal/git-profile.ps1
+++ config/terminal/git-profile.ps1
@@ -30,5 +30,5 @@ function log { git log $args }
 function tag { $msg = $args[1]; git tag -a $args[0] -m "$msg" }
 function pusha { $msg = $args; git add -u; git status; start-sleep 1; git commit -m "$msg"; git push }
 set-alias -name ch -value checkout
-set-alias -name stat -value status
+set-alias -name st -value status
 
