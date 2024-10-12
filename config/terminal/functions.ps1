6cc03340bb156f85e9077e1b4a64aca45e942657
akamienski <kamienski.adam@gmail.com>
2024-10-12 18:01:00 +0200

    updated functions.ps1

--- config/terminal/functions.ps1
+++ config/terminal/functions.ps1
@@ -3,13 +3,16 @@ function Test-Admin
     $user = [Security.Principal.WindowsIdentity]::GetCurrent();
     (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)  
 }
-
 function Set-Title 
 {
-    $repo = git rev-parse --show-toplevel 2>$null
-    if ($LASTEXITCODE -eq 0) {
-        $repo = Split-Path -Leaf $repo
-        $title = $repo + '@' + (git rev-parse --abbrev-ref HEAD 2>$null)
+    if (Get-Command git -ErrorAction SilentlyContinue) {
+        $repo = git rev-parse --show-toplevel 2>$null
+        if ($LASTEXITCODE -eq 0) {
+            $repo = Split-Path -Leaf $repo
+            $title = $repo + '@' + (git rev-parse --abbrev-ref HEAD 2>$null)
+        } else {
+            $title = Split-Path -Leaf (Get-Location)
+        }
     } else {
         $title = Split-Path -Leaf (Get-Location)
     }
