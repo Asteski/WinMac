02c15dcc843b7f0474bd24c4f59db9bae247f21b
Asteski <asteski@outlook.com>
2024-11-09 17:43:32 +0100

    updated install.ps1

--- pwsh/install.ps1
+++ pwsh/install.ps1
@@ -966,15 +966,13 @@ foreach ($app in $selectedApps) {
             if (-not (Test-Path "$Env:ProgramData\Windhawk\Engine\Mods")) {New-Item -ItemType Directory -Path "$Env:ProgramData\Windhawk\Engine\Mods" -Force | Out-Null}
             if (-not (Test-Path "$Env:ProgramData\Windhawk\ModsSource")) {New-Item -ItemType Directory -Path "$Env:ProgramData\Windhawk\ModsSource" -Force | Out-Null}
             # Stop-Process -name windhawk -force
-            Copy-Item ..\config\windhawk\Mods\* "$Env:ProgramData\Windhawk\Engine\Mods" -Recurse -Force
-            Copy-Item ..\config\windhawk\ModsSource\* "$Env:ProgramData\Windhawk\ModsSource" -Recurse -Force
+            Copy-Item ..\config\windhawk\* "$Env:ProgramData\Windhawk\*" -Recurse -Force
             reg import ..\config\windhawk\settings.reg > $null 2>&1
             Invoke-Output {Install-WinGetPackage -name Windhawk}
             $programsDir = "$($env:APPDATA)\Microsoft\Windows\Start Menu\Programs"
             Move-Item -Path "C:\Users\Public\Desktop\Windhawk.lnk" -Destination $programsDir -Force 
             # Start-Process -FilePath windhawk.exe -WorkingDirectory "$Env:ProgramFiles\Windhawk\"
-            Write-Host "Windhawk installation completed." -ForegroundColor Green
-            
+            Write-Host "Windhawk installation completed." -ForegroundColor Green   
         }
     # Other
         "11" {
