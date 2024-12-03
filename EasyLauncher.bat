80bdb654d78eeb0b84302aa0e55b86d0f46ccfc1
Asteski <asteski@outlook.com>
2024-12-03 22:25:12 +0100

    icon pack

--- EasyLauncher.bat
+++ EasyLauncher.bat
@@ -2,14 +2,17 @@
 cd /d "%~dp0\pwsh"
 if "%1"=="" (
     pwsh -NoProfile -ExecutionPolicy Bypass -File "install.ps1"
-) else (
-    if /i "%1"=="-uninstall" || /i "%1"=="-u" (
+) else if /i "%1"=="-uninstall" (
     pwsh -NoProfile -ExecutionPolicy Bypass -File "uninstall.ps1"
-) else (
-    if /i "%1"=="-nogui" || /i "%1"=="-n" (
+) else if /i "%1"=="-u" (
+    pwsh -NoProfile -ExecutionPolicy Bypass -File "uninstall.ps1"
+) else if /i "%1"=="-nogui" (
     pwsh -NoProfile -ExecutionPolicy Bypass -File "install.ps1" -nogui
-) else (
-    if /i "%1"=="-uninstallnogui" || /i "%1"=="-un"  (
+) else if /i "%1"=="-n" (
+    pwsh -NoProfile -ExecutionPolicy Bypass -File "install.ps1" -nogui
+) else if /i "%1"=="-uninstallnogui" (
+    pwsh -NoProfile -ExecutionPolicy Bypass -File "uninstall.ps1" -nogui
+) else if /i "%1"=="-un" (
     pwsh -NoProfile -ExecutionPolicy Bypass -File "uninstall.ps1" -nogui
 ) else (
     echo [31mInvalid argument.[0m
