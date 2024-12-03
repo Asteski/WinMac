d6c6121e4ca16e32cb1fea9431495af3169a0f3b
Asteski <asteski@outlook.com>
2024-12-03 17:31:52 +0100

    init push

--- pwsh/install.ps1
+++ pwsh/install.ps1
@@ -2,7 +2,7 @@ param (
     [switch]$noGUI,
     [switch]$debug
 )
-$version = "0.6.3"
+$version = "0.7.0"
 $date = Get-Date -Format "yy-MM-ddTHHmmss"
 $logFile = "WinMac_install_log_$date.txt"
 $transcriptFile = "WinMac_install_transcript_$date.txt"
--- pwsh/uninstall.ps1
+++ pwsh/uninstall.ps1
@@ -2,7 +2,7 @@ param (
     [switch]$noGUI,
     [switch]$debug
 )
-$version = "0.6.3"
+$version = "0.7.0"
 $date = Get-Date -Format "yy-MM-ddTHHmmss"
 $logFile = "WinMac_uninstall_log_$date.txt"
 $transcriptFile = "WinMac_uninstall_transcript_$date.txt"
