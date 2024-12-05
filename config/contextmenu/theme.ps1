0a6812a8b7a5c79d47ddf30fbd5cd7c4ffdd03c7
Asteski <asteski@outlook.com>
2024-12-05 20:21:53 +0100

    updated theme

--- config/contextmenu/theme.ps1
+++ config/contextmenu/theme.ps1
@@ -30,7 +30,29 @@ $themeStyle = (Get-ItemProperty -Path $registryPath0 -Name "GenThemeName").GenTh
 if ($mode -eq 'Light') {
 	$theme = $themeStyle -replace 'Dark', 'Light'
 	$dockRunningIndicator = $dockRunningIndicator -replace 'Dark', 'Light'
-
+    $curDestFolder = "C:\Windows\Cursors"
+    $RegConnect = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey([Microsoft.Win32.RegistryHive]"CurrentUser","$env:COMPUTERNAME")
+    $RegCursors = $RegConnect.OpenSubKey("Control Panel\Cursors",$true)
+    $RegCursors.SetValue("","Windows Black")
+    $RegCursors.SetValue("AppStarting","$curDestFolder\aero_black_working.ani")
+    $RegCursors.SetValue("Arrow","$curDestFolder\aero_black_arrow.cur")
+    $RegCursors.SetValue("Crosshair","$curDestFolder\aero_black_cross.cur")
+    $RegCursors.SetValue("Hand","$curDestFolder\aero_black_link.cur")
+    $RegCursors.SetValue("Help","$curDestFolder\aero_black_helpsel.cur")
+    $RegCursors.SetValue("IBeam","$curDestFolder\aero_black_beam.cur")
+    $RegCursors.SetValue("No","$curDestFolder\aero_black_unavail.cur")
+    $RegCursors.SetValue("NWPen","$curDestFolder\aero_black_pen.cur")
+    $RegCursors.SetValue("SizeAll","$curDestFolder\aero_black_move.cur")
+    $RegCursors.SetValue("SizeNESW","$curDestFolder\aero_black_nesw.cur")
+    $RegCursors.SetValue("SizeNS","$curDestFolder\aero_black_ns.cur")
+    $RegCursors.SetValue("SizeNWSE","$curDestFolder\aero_black_nwse.cur")
+    $RegCursors.SetValue("SizeWE","$curDestFolder\aero_black_ew.cur")
+    $RegCursors.SetValue("UpArrow","$curDestFolder\aero_black_up.cur")
+    $RegCursors.SetValue("Wait","$curDestFolder\aero_black_busy.ani")
+    $RegCursors.SetValue("Pin","$curDestFolder\aero_black_pin.cur")
+    $RegCursors.SetValue("Person","$curDestFolder\aero_black_person.cur")
+    $RegCursors.Close()
+    $RegConnect.Close()
 	Set-ItemProperty -Path $registryPath0 -Name "GenThemeName" -Value $theme
 	Set-ItemProperty -Path $registryPath0 -Name "BitmapsFolder" -Value "C:\Users\Public\Documents\WinStep\Themes\$theme\"
 	Set-ItemProperty -Path $registryPath0 -Name "GlobalBitmapFolder" -Value "C:\Users\Public\Documents\WinStep\Themes\$theme\"
