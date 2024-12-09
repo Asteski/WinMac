c49be7440404a31f7d54748607d5709f39cdfa66
Asteski <asteski@outlook.com>
2024-12-09 22:06:58 +0100

    updated install.ps1

--- pwsh/install.ps1
+++ pwsh/install.ps1
@@ -1031,7 +1031,7 @@ uint fWinIni);
             $regPath = "HKCU:\SOFTWARE\WinMac"
             if (-not (Test-Path -Path $regPath)) {New-Item -Path $regPath -Force | Out-Null}
             if ((Get-ItemProperty -Path $regPath -Name "Quick_Access" -ErrorAction SilentlyContinue).Icon_Pack -ne 1) {
-                Write-host "Pinning User folder, Programs and Recycle Bin to Quick Access..." -ForegroundColor DarkYellow
+                Write-Host "Pinning User folder, Programs and Recycle Bin to Quick Access..." -ForegroundColor DarkYellow
 $homeIni = @"
 [.ShellClassInfo]
 IconResource=C:\Windows\System32\SHELL32.dll,160
