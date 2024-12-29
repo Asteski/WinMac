6d80c70d780a5c2fb30a523ec3605c442c86ba6c
Asteski <asteski@outlook.com>
2024-12-29 12:51:38 +0100

    updated install.ps1

--- pwsh/install.ps1
+++ pwsh/install.ps1
@@ -1176,7 +1176,7 @@ IconResource=C:\WINDOWS\System32\imageres.dll,187
             Copy-Item -Path $sourceFilePath -Destination '..\temp\' -Force
             $appData = $env:LOCALAPPDATA -replace '\\', '\\'
             (Get-Content -Path $tempFilePath) -replace '%WINMACAPPDATA%', $appData | Set-Content -Path $tempFilePath
-            Get-ChildItem '..\temp\Add_Theme_Mode_in_Context_Menu.reg' | ForEach-Object { reg import $_.FullName > $null 2>&1 }
+            Get-ChildItem '..\temp\Add_Theme_Mode_in_Context_Menu.reg' | ForEach-Object { reg import $_.FullName }
         ## End Task
             Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Shell Extensions\Blocked" -Name "{470C0EBD-5D73-4d58-9CED-E91E22E23282}" -Value ""
             $taskbarDevSettings = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\TaskbarDeveloperSettings"
