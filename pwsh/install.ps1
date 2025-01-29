fbc8f5630f5099c5336e5b3097e8e70a9b0d1158
Asteski <asteski@outlook.com>
2025-01-29 15:27:42 +0100

    updated install.ps1

--- pwsh/install.ps1
+++ pwsh/install.ps1
@@ -1214,7 +1214,7 @@ IconResource=C:\WINDOWS\System32\imageres.dll,-87
             Copy-Item -Path $sourceFilePath -Destination '..\temp\' -Force
             $appData = $env:LOCALAPPDATA -replace '\\', '\\'
             (Get-Content -Path $tempFilePath) -replace '%WINMACAPPDATA%', $appData | Set-Content -Path $tempFilePath
-            reg import '..\config\contextmenu\add\Add_Theme_Mode_in_Context_Menu.reg'  > $null 2>&1
+            reg import '..\temp\Add_Theme_Mode_in_Context_Menu.reg'  > $null 2>&1
             reg import '..\config\contextmenu\add\Add_Hidden_items_to_context_menu.reg' > $null 2>&1
             Invoke-Output {winget uninstall "Windows web experience Pack" --silent}
         ## End Task
