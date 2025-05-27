d1cae37b66b90269683545e0ea745efc3bee4f46
Asteski <asteski@outlook.com>
2025-05-27 19:16:22 +0200

    updated install.ps1

--- pwsh/install.ps1
+++ pwsh/install.ps1
@@ -867,7 +867,7 @@ foreach ($app in $selectedApps) {
             $regBackup = Join-Path $extractFolder "Windhawk.reg"
             Stop-Process -n Windhawk -Force
             Copy-Item -Path $modsSourceBackup -Destination $windhawkRoot -Recurse -Force -ErrorAction SilentlyContinue
-            Copy-Item -Path config\windhawk\resource-redirect -Destination "$Env:LOCALAPPDATA\WinMac" -Recurse -Force -ErrorAction SilentlyContinue
+            Copy-Item -Path ..\config\windhawk\resource-redirect -Destination "$Env:LOCALAPPDATA\WinMac\" -Recurse -Force -ErrorAction SilentlyContinue
             $engineFolder = Join-Path $windhawkRoot "Engine"
             New-Item -ItemType Directory -Path $engineFolder -Force | Out-Null
             Copy-Item -Path $modsBackup -Destination $engineFolder -Recurse -Force -ErrorAction SilentlyContinue
