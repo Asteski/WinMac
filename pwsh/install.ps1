d3a2a6ec4b0942a0db2eef0a85f99ea88df667a9
Asteski <asteski@outlook.com>
2025-05-29 12:32:40 +0200

    updated install.ps1

--- pwsh/install.ps1
+++ pwsh/install.ps1
@@ -1245,7 +1245,7 @@ IconResource=C:\Windows\System32\SHELL32.dll,160
                     Remove-Item $homeIniFilePath -Force
                     New-Item -Path $homeIniFilePath -ItemType File -Force | Out-Null
                 }
-                Add-Content $homeIniFilePath -Value $homeIni
+                Add-Content $homeIniFilePath -Value $homeIni | Out-Null
                 (Get-Item $homeIniFilePath -Force).Attributes = 'Hidden, System, Archive'
                 (Get-Item $homeDir -Force).Attributes = 'ReadOnly, Directory'
                 $homePin = new-object -com shell.application
@@ -1260,20 +1260,20 @@ IconResource=C:\WINDOWS\System32\imageres.dll,-87
                 $programsIniFilePath = "$($programsDir)\desktop.ini"
                 if (Test-Path $programsIniFilePath)  {
                     Remove-Item $programsIniFilePath -Force
-                    New-Item -Path $programsIniFilePath -ItemType File -Force
+                    New-Item -Path $programsIniFilePath -ItemType File -Force | Out-Null
                 }
-                Add-Content $programsIniFilePath -Value $programsIni
+                Add-Content $programsIniFilePath -Value $programsIni | Out-Null
                 (Get-Item $programsIniFilePath -Force).Attributes = 'Hidden, System, Archive'
                 (Get-Item $programsDir -Force).Attributes = 'ReadOnly, Directory'
                 $programsPin = new-object -com shell.application
                 if (-not ($programsPin.Namespace($programsDir).Self.Verbs() | Where-Object {$_.Name -eq "pintohome"})) {
-                    $programsPin.Namespace($programsDir).Self.InvokeVerb("pintohome") 
+                    $programsPin.Namespace($programsDir).Self.InvokeVerb("pintohome") | Out-Null
                 }
                 $RBPath = 'HKCU:\Software\Classes\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\shell\pintohome\command\'
                 $name = "DelegateExecute"
                 $value = "{b455f46e-e4af-4035-b0a4-cf18d2f6f28e}"
-                New-Item -Path $RBPath -Force
-                New-ItemProperty -Path $RBPath -Name $name -Value $value -PropertyType String -Force
+                New-Item -Path $RBPath -Force | Out-Null
+                New-ItemProperty -Path $RBPath -Name $name -Value $value -PropertyType String -Force | Out-Null
                 $oShell = New-Object -ComObject Shell.Application
                 $recycleBin = $oShell.Namespace("shell:::{645FF040-5081-101B-9F08-00AA002F954E}")
                 if (-not ($recycleBin.Self.Verbs() | Where-Object {$_.Name -eq "pintohome"})) {
