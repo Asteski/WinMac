210a2a1bdf65a02ba62acd6df5a898ce8f899968
Asteski <asteski@outlook.com>
2025-06-06 11:38:22 +0200

    updated install.ps1

--- pwsh/install.ps1
+++ pwsh/install.ps1
@@ -480,33 +480,28 @@ if ($selectedApps -like '*4*' -or $selectedApps -like '*7*' -or $selectedApps -l
     if ($lightOrDark -eq "L" -or $lightOrDark -eq "l") {
         Write-Host "Using light theme." -ForegroundColor Green
         $stackTheme = 'light'
-        $recycleBinTheme = 'dark'
         $orbTheme = 'black'
     } elseif ($lightOrDark -eq "D" -or $lightOrDark -eq "d") {
         Write-Host "Using dark theme." -ForegroundColor Green
         $stackTheme = 'dark'
-        $recycleBinTheme = 'light'
         $orbTheme = 'white'
     } else {
         if ($windowsTheme -eq "Light") { 
             Write-Host "Invalid input. Defaulting to light theme." -ForegroundColor Yellow
             $stackTheme = 'light'
             $orbTheme = 'black'
-            $recycleBinTheme = 'dark'
             $lightOrDark = "L"
         }
         elseif ($windowsTheme -eq "Dark") {
             Write-Host "Invalid input. Defaulting to dark theme." -ForegroundColor Yellow
             $stackTheme = 'dark'
             $orbTheme = 'white'
-            $recycleBinTheme = 'light'
             $lightOrDark = "D"
         }
         else {
             Write-Host "Invalid input. Defaulting to light theme." -ForegroundColor Yellow
             $stackTheme = 'light'
             $orbTheme = 'black'
-            $recycleBinTheme = 'dark'
             $lightOrDark = "L"
         }
     }
@@ -1328,14 +1323,10 @@ IconResource=C:\WINDOWS\System32\imageres.dll,-87
                 }
                 Copy-Item -Path $_.FullName -Destination $filePath -Force
             }
-            try {
-                Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" -Name "(default)" -Value "$env:LOCALAPPDATA\WinMac\Icons\recycle_bin_empty_$recycleBinTheme.ico"
-                Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" -Name "empty" -Value "$env:LOCALAPPDATA\WinMac\Icons\recycle_bin_empty_$recycleBinTheme.ico"
-                Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" -Name "full" -Value "$env:LOCALAPPDATA\WinMac\Icons\recycle_bin_full_$recycleBinTheme.ico"
-            }
-            catch {
-                Write-Host "Error details: $($_.Exception.Message)" -ForegroundColor Red
-            }
+            $recycleBinTheme = if ($lightOrDark -eq "L" -or $lightOrDark -eq "l") {"dark"} else {"light"}
+            Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" -Name "(default)" -Value "$env:LOCALAPPDATA\WinMac\Icons\recycle_bin_empty_$recycleBinTheme.ico"
+            Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" -Name "empty" -Value "$env:LOCALAPPDATA\WinMac\Icons\recycle_bin_empty_$recycleBinTheme.ico"
+            Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" -Name "full" -Value "$env:LOCALAPPDATA\WinMac\Icons\recycle_bin_full_$recycleBinTheme.ico"
             $oShell = New-Object -ComObject Shell.Application
             $recycleBin = $oShell.Namespace("shell:::{645FF040-5081-101B-9F08-00AA002F954E}")
             if (-not ($recycleBin.Self.Verbs() | Where-Object {$_.Name -eq "pintohome"})) {
