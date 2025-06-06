ba03ba47ce61737b9c75240761d34aecb7dc4e53
Asteski <asteski@outlook.com>
2025-06-06 21:20:21 +0200

    updated theme.ps1

--- config/reg/theme.ps1
+++ config/reg/theme.ps1
@@ -111,5 +111,5 @@ Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\P
 if ($mode2 -eq 'App') {
 	Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme" -Type DWord -Value $OSMode -ErrorAction SilentlyContinue
 }
-Start-Process explorer -ErrorAction SilentlyContinue | Out-Null
-Start-Process "C:\Program Files (x86)\Winstep\Nexus.exe" -ErrorAction SilentlyContinue | Out-Null
+Start-Process explorer > $null 2>&1
+Start-Process "C:\Program Files (x86)\Winstep\Nexus.exe" > $null 2>&1
