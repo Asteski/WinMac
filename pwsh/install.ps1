13dd0338b7a084289e8468c21074d287d27ef31f
Asteski <asteski@outlook.com>
2025-07-14 12:22:21 +0200

    bumped ver nr

--- pwsh/install.ps1
+++ pwsh/install.ps1
@@ -1,7 +1,7 @@
 param (
     [switch]$noGUI
 )
-$version = "1.1.1"
+$version = "1.2.0"
 $ErrorActionPreference = "SilentlyContinue"
 $WarningPreference = "SilentlyContinue"
 $ProgressPreference = "SilentlyContinue"
