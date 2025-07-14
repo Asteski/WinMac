d4ea5f35657e2739093dabb62c1e5b54485d4fb5
Asteski <asteski@outlook.com>
2025-07-14 12:14:33 +0200

    updated version nr

--- pwsh/install.ps1
+++ pwsh/install.ps1
@@ -1,7 +1,7 @@
 param (
     [switch]$noGUI
 )
-$version = "1.1.0"
+$version = "1.1.1"
 $ErrorActionPreference = "SilentlyContinue"
 $WarningPreference = "SilentlyContinue"
 $ProgressPreference = "SilentlyContinue"
