425b7bddca84c9145d8acc6166f0692f1b08c4d1
Asteski <asteski@outlook.com>
2025-08-09 00:31:30 +0200

    fix prompt showing ~ when in folder with the same name as user name

--- config/terminal/macos-prompt.ps1
+++ config/terminal/macos-prompt.ps1
@@ -13,7 +13,7 @@ function prompt {
     $folder = Split-Path -Leaf $pwd
     $computerName = hostname
     if (Test-Admin -eq $true) { $userName = "`e[91m$($userName)`e[0m" } else { $userName = "`e[93m$($userName)`e[0m" }
-    if ($folder -eq $env:USERNAME)
+    if ($env:USERPROFILE -eq $pwd)
     {
         "$userName@$computerName ~ % "
     }
