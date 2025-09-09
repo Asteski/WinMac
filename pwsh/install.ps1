eddbaf9f1dd26cb4d1038e39ffb2e5765a5c24d4
Asteski <asteski@outlook.com>
2025-09-10 00:58:16 +0200

    updated install.ps1

--- pwsh/install.ps1
+++ pwsh/install.ps1
@@ -657,8 +657,8 @@ if ($null -eq $wingetCliCheck) {
 else {
     Write-Host "Winget installation completed." -ForegroundColor Green
 }
-#* Copy Common Resources files
-Write-Host "Copying Common Resource files to Windows directory..." -ForegroundColor Yellow
+#* Copy common resource files to Windows directory
+Write-Host "Copying common resource files to Windows directory..." -ForegroundColor Yellow
 New-Item -ItemType Directory -Path "$ENV:WINDIR\Resources\Scripts" -Force | Out-Null
 takeown /F "$env:WINDIR\Resources" /A /R /D Y > $null 2>&1
 icacls "$env:WINDIR\Resources" /grant Administrators:F /T > $null 2>&1
@@ -675,7 +675,7 @@ Expand-Archive -Path "$wmTemp\wallpapers.zip" -DestinationPath "$ENV:WINDIR\Web\
 Remove-Item "$wmTemp\wallpapers.zip" -Force
 icacls "$env:WINDIR\Resources" /inheritance:e /T > $null 2>&1
 icacls "$env:WINDIR\Web" /inheritance:e /T > $null 2>&1
-Write-Host "Copying Common Resource files to Windows directory completed." -ForegroundColor Green
+Write-Host "Copying common resource files to Windows directory completed." -ForegroundColor Green
 Write-Host "`n-----------------------------------------------------------------------`n" -ForegroundColor Cyan
 #! WinMac deployment
 #* Install WinMac components
@@ -1507,6 +1507,7 @@ IconResource=C:\Windows\Resources\Icons\programs.ico
                 Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "Wallpaper" -Value "$ENV:WINDIR\Web\Wallpaper\Surface\img0.jpg"
             }
             if (Get-Process -n SystemSettings -ErrorAction SilentlyContinue) { Stop-Process -n SystemSettings -Force }
+        Write-Host "Other settings deployment completed." -ForegroundColor Green
         }
     }
 }
