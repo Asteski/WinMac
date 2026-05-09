8a72465356601681121538a2dece43703f5b7439
Asteski <asteski@outlook.com>
2026-05-09 19:08:25 +0200

    updated wmm config

--- pwsh/install.ps1
+++ pwsh/install.ps1
@@ -1388,10 +1388,10 @@ IconResource=C:\WINDOWS\System32\imageres.dll,-87
             reg import '..\config\registry\add\Add_Navigation_pane_to_context_menu.reg' > $null 2>&1
             reg import '..\temp\Add_Theme_Mode_in_Context_Menu.reg' > $null 2>&1
             New-Item -Path "$env:WINDIR\Resources\Icons" -ItemType Directory -Force | Out-Null
-            & "$env:ProgramFiles\7-Zip\7z.exe" x "..\config\resources\Icons.7z" "-o$env:WINDIR\Resources\Icons" -y > $null 2>&1
-            & "$env:ProgramFiles\7-Zip\7z.exe" x "..\config\resources\System32.7z" "-o$env:WINDIR\System32" -y > $null 2>&1
-            & "$env:ProgramFiles\7-Zip\7z.exe" x "..\config\resources\Themes.7z" "-o$env:WINDIR\Resources\Themes" -y > $null 2>&1
-            & "$env:ProgramFiles\7-Zip\7z.exe" x "..\config\resources\Wallpapers.7z" "-o$env:WINDIR\Web\Wallpaper" -y > $null 2>&1
+            Expand-Archive -Path "..\config\resources\Icons.zip"  -DestinationPath "$env:WINDIR\Resources\Icons" -Force
+            Expand-Archive -Path "..\config\resources\System32.zip"  -DestinationPath "$env:WINDIR\System32" -Force
+            Expand-Archive -Path "..\config\resources\Themes.zip"  -DestinationPath "$env:WINDIR\Resources\Themes"  -Force
+            Expand-Archive -Path "..\config\resources\Wallpapers.zip"  -DestinationPath "$env:WINDIR\Web\Wallpaper" -Force
             New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowIconOverlay" -Value 0 -PropertyType DWord -Force | Out-Null
         #? End Task
             Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Shell Extensions\Blocked" -Name "{470C0EBD-5D73-4d58-9CED-E91E22E23282}" -Value "" 
